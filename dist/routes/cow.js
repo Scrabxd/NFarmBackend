7ad0995c366e80a6d2aa4c9e39487a0a806bffec
Luis Fernando Cabral <scrab_2877@hotmail.com>
2022-11-22 20:06:34 -0600

    Get Single cow

--- dist/routes/cow.js
+++ dist/routes/cow.js
@@ -18,6 +18,10 @@ cow.get('/', [
     middlewares_1.validateAPIKey,
     validation_1.validation
 ], controllers_1.getCows);
+cow.get('/single', [
+    middlewares_1.validateAPIKey,
+    validation_1.validation
+], cow_1.getSingleCow);
 cow.put('/', [
     middlewares_1.validateAPIKey,
     validation_1.validation
